026ef98a45870d49714764681463cd59243c4c86
Mehdi Shirazi <msh199271@gmail.com>
2020-02-12 17:10:03 +0330

    Added new release

--- maestro.js
+++ maestro.js
@@ -1,7 +1,7 @@
 let Token;
+let project;
 
-
-async function FetchAPI(url, method='LIST', data = {}, headers={}) {
+async function FetchAPI(url, method='GET', data = {}, headers={}) {
   // Default options are marked with *
   const response = await fetch(url, {
     method: method, // *GET, POST, PUT, DELETE, etc.
@@ -17,19 +17,50 @@ async function FetchAPI(url, method='LIST', data = {}, headers={}) {
     referrerPolicy: 'no-referrer', // no-referrer, *client
     //body: JSON.stringify(data) // body data type must match "Content-Type" header
   });
-  let x = await response.json();
-  return x
+    resp = await response.json();
+  return resp
 }
 
 
 async function Login(){
     Token = document.getElementById('name').value;
-    posts = await FetchAPI('https://milestone-maestro.carrene.com/apiv1/projects?');
-    console.log(posts);
+    project = await FetchAPI('https://milestone-maestro.carrene.com/apiv1/projects?id=10', "LIST");
+    console.log(project);
     let elem = document.getElementById('input');
     elem.parentNode.removeChild(elem); 
-    issue_show()
+    project_show()
 }
 
 function project_show(){    
+    let project_length = project.length;
+    for(i=0; i < project_length; i++){
+        let div_elem = document.createElement("div");
+        div_elem.id = 'd' + i;
+        div_elem.className = "id";
+        document.getElementById('mehdi').appendChild(div_elem);
+        CreateCardProject(div_elem, i);
+    }
 }
+
+function CreateCardProject(currentDiv, projectId){
+    let idContent = document.createElement("p");
+    currentDiv.appendChild(idContent);
+    idContent.innerText = "ID: " + project[projectId].id; 
+
+    let titleContent = document.createElement("p");
+    currentDiv.appendChild(titleContent);
+    titleContent.id = "pTitle" + projectId;
+    titleContent.innerText = "Title: " + project[projectId].title;
+
+    let tempoContent = document.createElement("p");
+    currentDiv.appendChild(tempoContent);
+    tempoContent.innerText = "Tempo: " + project[projectId].boarding; 
+}
+
+function SelectProject(e){
+    let P1 = document.getElementById('d0');
+    let P2 = document.getElementById('d1');
+    let P3 = document.getElementById('d2');
+    let P4 = document.getElementById('d3');
+}
+
